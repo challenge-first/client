893629be67e1538f53f311e70fee172211cdfa16
kki4504 <rhkddlf9876@gmail.com>
2023-08-17 02:46:33 +0900

    feat: 상품 4개 표시

---
+++ js/index.js
@@ -0,0 +1,59 @@
+const url = "http://localhost:8080/products/main";
+
+const ul = document.querySelector(".product-item-list");
+
+// 메인으로 돌아왔을 때 productId 초기화
+(function () {
+    localStorage.removeItem("productId");
+})();
+
+// api 호출 후, 4개 상품 출력
+const getTop4Products = async () => {
+    try {
+        const response = await axios({ method: "get", url });
+        console.log(response.data.data);
+        let templi = ``;
+        response.data.data.forEach(({ id, imageUrl, name, price }) => {
+            templi =
+                templi +
+                `
+                <li class="product-item">
+                    <div onclick="onClickProduct(${id})">
+                        <div class="product-img-area">
+                            <img
+                                class="product-img"
+                                src="${imageUrl}"
+                            />
+                        </div>
+                        <div class="product-info-area">
+                            <div class="product-info-left-area">
+                                <p class="product-title">
+                                    ${name}
+                                </p>
+                            </div>
+                            <div class="product-info-right-area">
+                                <!-- 꽉찬 하트는 i 태그 이름을 xi-heart 이걸로 하시면 됩니다. -->
+                                <form action="">
+                                    <button class="like-btn">
+                                        <i class="xi-heart-o"></i>
+                                    </button>
+                                </form>
+                                <p class="product-price">${price}</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            `;
+        });
+        ul.innerHTML = templi;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const onClickProduct = (id) => {
+    localStorage.setItem("productId", id);
+    window.location = "/pages/product.html";
+};
+
+getTop4Products();
--- js/login.js
+++ js/login.js
@@ -3,37 +3,42 @@ const url = "http://localhost:8080/members/login";
 const loginForm = document.querySelector(".login-form");
 
 const dataToSend = {
-  username: "",
-  password: "",
+    username: "",
+    password: "",
 };
 
 const onIdInput = (event) => {
-  dataToSend.username = event.target.value;
-  console.log(dataToSend.username);
+    dataToSend.username = event.target.value;
+    console.log(dataToSend.username);
 };
 
 const onPasswordInput = (event) => {
-  dataToSend.password = event.target.value;
-  console.log(dataToSend.password);
+    dataToSend.password = event.target.value;
+    console.log(dataToSend.password);
 };
 
 const id = document.querySelector("#id").addEventListener("input", onIdInput);
 const pwd = document
-  .querySelector("#password")
-  .addEventListener("input", onPasswordInput);
+    .querySelector("#password")
+    .addEventListener("input", onPasswordInput);
 const button = document.querySelector("#button");
 
 button.addEventListener("click", async () => {
-  console.log("dataToSend:", dataToSend);
-  axios
-    .post(url, dataToSend)
-    .then(async (res) => {
-      const data = await res.data;
-      if (res.headers.authorization) {
-        localStorage.setItem("authorization", res.headers.authorization);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    console.log("dataToSend:", dataToSend);
+    axios
+        .post(url, dataToSend)
+        .then(async (res) => {
+            const data = await res.data;
+            if (res.headers.authorization) {
+                localStorage.setItem(
+                    "authorization",
+                    res.headers.authorization
+                );
+            }
+            console.log(data);
+            document.location = "/index.html";
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
