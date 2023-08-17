d137f754602b68116c846bebc514ce951943fd86
kimdonghyun <ssk0080@gmail.com>
2023-08-17 11:29:48 +0900

    feat: 검색

--- js/search.js
+++ js/search.js
@@ -2,7 +2,7 @@ const laptop = document.querySelector(".category-filter-laptop");
 const computerParts = document.querySelector(".category-filter-computer-parts");
 const submitBtn = document.querySelector(".category-search-btn");
 
-let currentMainCategory;
+let currentMainCategory = "LAPTOP";
 let currentSubCategory = [];
 
 if (localStorage.getItem("searchResult") !== null) {
@@ -11,14 +11,13 @@ if (localStorage.getItem("searchResult") !== null) {
         <li class="product-item">
             <a href="/pages/product.html">
                 <div class="product-img-area">
-                    <img class="product-img" src="/images/product.jpg">
+                    <img class="product-img" src="${item.imageUrl}">
                 </div>
                 <div class="product-info-area">
                     <div class="product-info-left-area">
                         <p class="product-title">${item.name}</p>
                     </div>
                     <div class="product-info-right-area">
-                        <!-- 꽉찬 하트는 i 태그 이름을 xi-heart 이걸로 하시면 됩니다. -->
                         <form action=""><button class="like-btn"><i class="xi-heart-o"></i></button></form>
                         <p class="product-price">${item.price}</p>
                     </div>
@@ -27,6 +26,17 @@ if (localStorage.getItem("searchResult") !== null) {
         </li>
         `
     }
+}
+
+if (localStorage.getItem("searchResult") == null) {
+    let url = `http://localhost:8080/products/maincategory/${currentMainCategory}`;
+    
+    axios.get(url)
+    .then(data => {
+        localStorage.setItem("searchResult", JSON.stringify(data.data.data))
+        location.reload();
+    })
+    .catch(error => console.log(error))
 }
 
 laptop.addEventListener("click", () => {
@@ -96,3 +106,7 @@ document.querySelector(".search-form").addEventListener("submit", async (event) 
     })
     .catch(error => console.log(error))
 })
+
+if (localStorage.getItem("searchResult") !== null) {
+    localStorage.removeItem("searchResult");
+}
