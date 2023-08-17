01781208365e9a35169aa5d4d3b713811a6a11f9
Sihwan Yu <tlghks1217@naver.com>
2023-08-17 22:50:48 +0900

    feat: 경매 조회, 입찰

--- js/auction.js
+++ js/auction.js
@@ -1,17 +1,31 @@
-const div = document.querySelector(".auction-detail");
+const div = document.querySelector(".auction-info");
 const url = "http://localhost:8080/auctions";
 let tempDiv = ``;
+let auctionId;
+let buttonElement;
+let pointElement;
 
-
-
-(async function () {
+const getAuction = async () => {
     try {
-        const response = await axios({ method: "get", url })
+        const response = await axios({
+            method: "get",
+            url: url,
+            headers: {
+                Authorization: localStorage.getItem("authorization"),
+            },
+        });
         console.log(response.data.data);
-        const { id, name, imageUrl, openingPrice, winningPrice, openingTime, closingTime } = response.data.data;
-        const auctionId = (id) => {
-            localStorage.setItem('auctionId', id);
-        }
+        const {
+            id,
+            name,
+            imageUrl,
+            openingPrice,
+            winningPrice,
+            openingTime,
+            closingTime,
+        } = response.data.data;
+        localStorage.setItem("auctionId", id);
+
         tempDiv = `
                 <div class="auction-title-area">
                     <p class = "auction-title">
@@ -29,78 +43,65 @@ let tempDiv = ``;
                     <p class="opening-time">시작 시간: <span class="auction-start-time">${openingTime}</span></p>
                     <p class="closing-time">마감 시간: <span class="auction-end-time">${closingTime}</span></p>
                 </div>
-                <form class="auction-btn-area" action="">
-                    <input id="point" type="number" placeholder="입찰 금액">
-                    <button id="button" type="button">입 찰</button>
-                </form>
-                `
+                `;
         div.innerHTML = tempDiv;
     } catch (err) {
         console.log(err);
     }
-})(); 
-
-const auctionId = localStorage.getItem("auctionId");
-const postUrl = "http://localhost:8080/auctions/" + auctionId;
-const bidform = document.querySelector(".auction-btn-area")
-
-const instance = axios.create({
-    baseURL: postUrl
-    // timeout: 2000,
-    });
-    
-    instance.interceptors.request.use(
-    // 요청을 보내기 전 수행되는 함수
-    //매개변수 이름은 config가 아니라도 됨
-    function (config) {
-    let accesstoken = localStorage.getItem("authorization");
-    config.headers.Authorization = accesstoken;
-    return config;
-    },
-    //오류 요청을 보내기 전 수행되는 함수
-    function (error) {
-    return Promise.reject(error);
-    }
-    );
-    
-    instance.interceptors.response.use(
-    // 응답을 보내기 전 수행되는 함수
-    function (response) {
-    return response;
-    },
-    //오류응답을 보내기 전 수행되는 함수
-    function (error) {
-    return Promise.reject(error);
-    }
-    );
-
-const dataToSend = {
-    point:"",
-    currentTime:""
+};
+const buttonForm = () => {
+    let tempForm = `
+                <form class="auction-btn-area" action="">
+                    <input id="point" type="number" placeholder="입찰 금액">
+                    <button id="button" class="bid-button" type="button">입 찰</button>
+                </form>
+                `;
 };
 
-const onPointInput = (event) => {
-    dataToSend.point = event.target.value;
-    console.log(dataToSend.point);
-};
+const bid = () => {
+    const dataToSend = {
+        point: "",
+        time: "",
+    };
 
-const onCurrentTime = (event) => {
-    dataToSend.currentTime = new Date().getTime();
-}
+    auctionId = localStorage.getItem("auctionId");
+    const postUrl = "http://localhost:8080/auctions/" + auctionId;
 
-const point = document.querySelector("#point").addEventListener("input", onPointInput);
-const currentTime = document.querySelector("#point").addEventListener("input", onCurrentTime);
-const button = document.querySelector("#button");
+    const onPointInput = (event) => {
+        dataToSend.point = event.target.value;
+        dataToSend.time = new Date().toISOString();
+    };
 
-button.addEventListener("click", async () => {
-    console.log("dataToSend", dataToSend);
-    axios
-        .post(postUrl, dataToSend)
-        .then(async (res) => {
-            const data = await res.data;
-            console.log(data);
+    buttonElement = document.querySelector("#button");
+    pointElement = document
+        .querySelector("#point")
+        .addEventListener("input", onPointInput);
+
+    buttonElement.addEventListener("click", () => {
+        axios({
+            method: "post",
+            url: postUrl,
+            data: dataToSend,
+            headers: {
+                Authorization: localStorage.getItem("authorization"),
+            },
         })
-        .catch((err) => {
-            console.log(err)
-        });
-});
+            .then((res) => {
+                const data = res.data;
+                alert("입찰성공");
+                console.log(data);
+            })
+            .catch((err) => {
+                alert("에러");
+                console.log(err);
+            });
+    });
+};
+
+(async function () {
+    await getAuction();
+    bid();
+    setInterval(async () => {
+        await getAuction();
+    }, 20000);
+})();
